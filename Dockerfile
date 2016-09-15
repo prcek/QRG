5e12a7a2d20214f54770f3932b07f9d16999545f
Tomas Hluchan <tomas.hluchan@gmail.com>
2016-09-15 18:35:05 +0200

    qt img test

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@
 FROM tiangolo/uwsgi-nginx-flask:flask
 
 RUN pip install reportlab
+RUN pip install qrcode
 
 COPY ./app /app
 
--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from flask import Flask
 from flask import make_response
 from reportlab.pdfgen import canvas
+import base64
+from io import BytesIO
+
+import qrcode as qrc
 
 app = Flask(__name__)
 
@@ -25,6 +29,38 @@ def pdf():
     response.headers['Content-Disposition'] = "attachment; filename='sakulaci.pdf"
     response.mimetype = 'application/pdf'
     return response
+
+
+correction_levels = {
+    'L': qrc.constants.ERROR_CORRECT_L,
+    'M': qrc.constants.ERROR_CORRECT_M,
+    'Q': qrc.constants.ERROR_CORRECT_Q,
+    'H': qrc.constants.ERROR_CORRECT_H
+}
+
+
+
+@app.route("/qr")
+def qccode():
+    qr = qrc.QRCode(
+        version=None,
+        error_correction=correction_levels['M'],
+        box_size=10,
+        border=4
+    )
+    qr.add_data(250*"a")
+    qr.make(fit=True)
+
+    # creates qrcode base64
+    out = BytesIO()
+    qr_img = qr.make_image()
+    qr_img.save(out, 'PNG')
+
+    img_out = out.getvalue()	
+    response = make_response(img_out)
+    response.mimetype = 'image/png'
+    return response
+
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0', debug=True, port=80)
